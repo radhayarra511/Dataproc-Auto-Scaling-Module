50c2732b56bb96940eb3ea562f5dea994da504b6
yarraradha <43906675+yarraradha@users.noreply.github.com>
2023-01-16 17:58:41 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,12 @@
+output "id" {
+  description = "returns a string"
+  value       = google_dataproc_autoscaling_policy.asp.id
+}
+
+output "name" {
+  description = "returns a string"
+  value       = google_dataproc_autoscaling_policy.asp.name
+}
+
+
+
